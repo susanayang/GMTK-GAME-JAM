ad5031271571fb3f767edabbf68628eefcc600e3
stickbrime <jnwwwwwww@outlook.com>
2025-07-31 16:15:35 +0800

    player

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject player;
+    public float movespeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += (Vector3.up * movespeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += (Vector3.left * movespeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += (Vector3.down * movespeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += (Vector3.right * movespeed) * Time.deltaTime;
+        }
+    }
+}
