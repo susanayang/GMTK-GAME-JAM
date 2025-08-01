5d00ca8e5ad31c4e9fd2bcf9841fdda7238ce657
stickbrime <jnwwwwwww@outlook.com>
2025-08-01 13:41:07 +0800

    Update camerafitscript.cs

--- Assets/scripts/camerafitscript.cs
+++ Assets/scripts/camerafitscript.cs
@@ -8,7 +8,7 @@ public class camerafitscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.main.aspect = (float)Screen.width / Screen.height;
     }
 
     // Update is called once per frame
